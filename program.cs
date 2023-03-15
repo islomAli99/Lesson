a4ef6f8bb4da71c8df284c9eb1ccc9dfe61d43f8
islomAli99 <109507380+islomAli99@users.noreply.github.com>
2023-03-15 11:13:17 +0500

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,1 +1,27 @@
-Console.writeLine("Hello World");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number");
+          int num1 = Convert.ToInt32(Console.ReadLine());
+          Console.Write("Enter another number");
+          int num2 = Convert.ToInt32(Console.ReadLine());
+          
+          int sum = num1 + num2;
+          
+          
+          Console.WriteLine("Total of sum: " +` sum);
+          
+          
+          
+        }
+    }
+}
